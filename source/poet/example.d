05b715b9279ac76e7ecf3de13c28bda0f637a4e5
outlandkarasu <outland.karasu@gmail.com>
2019-12-25 17:40:31 +0900

    issue #13 add example value.

--- source/poet/example.d
+++ source/poet/example.d
@@ -4,6 +4,7 @@ example type module.
 module poet.example;
 
 import poet.type : IType, Type;
+import poet.value : IValue, Value;
 
 @safe:
 
@@ -15,6 +16,19 @@ final immutable class CExample : IType
     override bool equals(scope Type other) @nogc nothrow pure scope
     {
         return this is other;
+    }
+
+    /**
+    create example value.
+
+    Returns:
+        example value.
+    */
+    ExampleValue createValue() nothrow pure scope
+    out (r; r !is null)
+    out (r; r.type is this)
+    {
+        return new ExampleValue(this);
     }
 
 private:
@@ -50,8 +64,45 @@ pure nothrow unittest
     assert(!u.equals(t));
 }
 
+///
+pure nothrow unittest
+{
+    immutable t = example();
+    immutable v1 = t.createValue();
+    assert(v1.type is t);
+
+    immutable v2 = t.createValue();
+    assert(v2.type is t);
+    assert(v1 !is v2);
+}
+
 /**
 Immutable Example.
 */
 alias Example = immutable(CExample);
+
+private:
+
+/**
+Example value class.
+*/
+final immutable class CExampleValue : IValue
+{
+    @property Example type() @nogc nothrow pure scope
+    {
+        return type_;
+    }
+
+private:
+
+    this(Example e) @nogc nothrow pure scope
+    in (e !is null)
+    {
+        this.type_ = e;
+    }
+
+    Example type_;
+}
+
+alias ExampleValue = immutable(CExampleValue);
 
