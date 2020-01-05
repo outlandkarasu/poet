a123ea9af2499bf262222728a6ff9ff825141b3d
outlandkarasu <outland.karasu@gmail.com>
2020-01-05 17:18:33 +0900

    issue #20 implement create function instruction.

--- source/poet/instruction/create_function.d
+++ source/poet/instruction/create_function.d
@@ -3,8 +3,8 @@ Create function instruction module.
 */
 module poet.instruction.create_function;
 
-import poet.context : Context, Variable;
-import poet.fun : FunctionType;
+import poet.context : Context, ScopeID, Variable;
+import poet.fun : FunctionType, FunctionValue;
 import poet.instruction.instruction : Instruction, IInstruction;
 
 @safe:
@@ -14,24 +14,64 @@ create function instruction.
 */
 final immutable class CCreateFunctionInstruction : IInstruction
 {
-    this(FunctionType type, Instruction[] instructions, Variable result)
+    /**
+    Constructor.
+
+    Params:
+        type = function type.
+        instructions = function instructions.
+        result = function result variable.
+        scopeID = function scope ID.
+    */
+    this(
+        FunctionType type,
+        Instruction[] instructions,
+        Variable result,
+        ScopeID scopeID) @nogc nothrow pure scope
     in (type !is null)
     {
         this.type_ = type;
         this.instructions_ = instructions;
         this.result_ = result;
+        this.scopeID_ = scopeID;
     }
 
     override void execute(scope Context context) pure scope
     in (false)
     {
+        immutable value = new FunctionValue(type_, instructions_, result_, scopeID_, context.save);
+        context.push(value);
     }
 
 private:
     FunctionType type_;
     Instruction[] instructions_;
     Variable result_;
+    ScopeID scopeID_;
 }
+
+///
+pure unittest
+{
+    import poet.context : VariableIndex;
+    import poet.example : example;
+    import poet.fun : funType;
+
+    immutable t = example();
+    immutable f = funType(t, t);
+
+    auto c = new Context();
+    immutable cfi = new CreateFunctionInstruction(f, [], Variable(ScopeID(1), VariableIndex.init), ScopeID(1));
+    cfi.execute(c);
+
+    immutable fv = cast(FunctionValue) c.get(Variable(c.scopeID, VariableIndex(1)));
+    assert(fv !is null);
+    assert(fv.type.equals(f));
+
+    immutable tv = t.createValue();
+    assert(fv.execute(tv) is tv);
+}
+
 
 alias CreateFunctionInstruction = immutable(CCreateFunctionInstruction);
 
