535a264dfa082ab4d9660b625d803f88f3c7dd49
outlandkarasu <outland.karasu@gmail.com>
2020-01-05 17:10:09 +0900

    issue #20 implement function execution.

--- source/poet/fun.d
+++ source/poet/fun.d
@@ -3,8 +3,11 @@ Function type module.
 */
 module poet.fun;
 
+import std.exception : enforce;
+
 import poet.instruction : Instruction;
-import poet.context : Variable;
+import poet.context : Context, SavePoint, ScopeID, Variable;
+import poet.exception : UnmatchTypeException;
 import poet.type : IType, Type;
 import poet.value : IValue, Value;
 
@@ -193,37 +196,82 @@ final immutable class CFunctionValue : IValue
         type = function type.
         instructions = function instructions.
         result = function result variable.
+        scopeID = function scope ID.
+        startPoint = context start point.
     */
-    this(FunctionType type, Instruction[] instructions, Variable result)
+    this(
+        FunctionType type,
+        Instruction[] instructions,
+        Variable result,
+        ScopeID scopeID,
+        SavePoint startPoint) @nogc nothrow pure scope
     in (type !is null)
     {
         this.type_ = type;
         this.instructions_ = instructions;
         this.result_ = result;
+        this.scopeID_ = scopeID;
+        this.startPoint_ = startPoint;
     }
 
-    @property @nogc nothrow pure scope
+    override @property FunctionType type() @nogc nothrow pure scope
     {
-        override FunctionType type()
+        return type_;
+    }
+
+    /**
+    Execute function.
+
+    Params:
+        argument = function argument.
+    Returns:
+        function result.
+    */
+    Value execute(Value argument) pure scope
+    out (r; r !is null && r.type.equals(type_.result))
+    {
+        enforce!UnmatchTypeException(argument.type.equals(type_.argument));
+        auto c = new Context(startPoint_);
+        c.pushScope(scopeID_, argument);
+
+        foreach (i; instructions_)
         {
-            return type_;
+            i.execute(c);
         }
 
-        Instruction[] instructions()
-        {
-            return instructions_;
-        }
-
-        ref const(Variable) result() return
-        {
-            return result_;
-        }
+        immutable resultValue = c.get(result_);
+        enforce!UnmatchTypeException(resultValue.type.equals(type_.result));
+        return resultValue;
     }
 
 private:
     FunctionType type_;
     Instruction[] instructions_;
     Variable result_;
+    ScopeID scopeID_;
+    SavePoint startPoint_;
+}
+
+///
+pure unittest
+{
+    import std.exception : assertThrown;
+
+    import poet.context : VariableIndex;
+    import poet.example : example;
+
+    immutable t = example();
+    immutable f = funType(t, t);
+
+    auto c = new Context();
+    immutable fv = new FunctionValue(f, [], Variable(ScopeID(1), VariableIndex.init), ScopeID(1), c.save);
+
+    immutable tv = t.createValue();
+    assert(fv.execute(tv) is tv);
+
+    // unmatch argument type.
+    immutable uv = example().createValue();
+    assertThrown!UnmatchTypeException(fv.execute(uv));
 }
 
 alias FunctionValue = immutable(CFunctionValue);
