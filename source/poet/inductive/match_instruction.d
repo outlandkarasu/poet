3f77708bbb2f4f693dfc9715db5f1622433a5ca4
outlandkarasu <outland.karasu@gmail.com>
2020-01-27 22:45:46 +0900

    issue #19 fix match instruction execution.

--- source/poet/inductive/match_instruction.d
+++ source/poet/inductive/match_instruction.d
@@ -3,11 +3,11 @@ Match instruction.
 */
 module poet.inductive.match_instruction;
 
-import std.exception : enforce;
+import std.exception : basicExceptionCtors, enforce;
 import std.typecons : Rebindable;
 
 import poet.context : Context, next, ScopeID, Variable, VariableIndex;
-import poet.exception : UnmatchTypeException;
+import poet.exception : PoetException, UnmatchTypeException;
 import poet.fun : FunctionType, FunctionValue;
 import poet.inductive.value : InductiveValue;
 import poet.instruction.instruction : Instruction, IInstruction;
@@ -20,44 +20,18 @@ Match instruction.
 */
 final immutable class CMatchInstruction : IInstruction
 {
-    /**
-    Constructor.
-
-    Params:
-        context = current context
-        selfFunctionType = self function type
-        argument = match argument
-        caseFunctions = case function variables
-    */
-    this(
-        scope Context context,
-        FunctionType selfFunctionType,
-        Variable argument,
-        immutable(Variable)[] caseFunctions) nothrow pure
-    in (selfFunctionType !is null)
-    {
-        this.argument_ = argument;
-        this.caseFunctions_ = caseFunctions;
-
-        immutable nextScopeID = context.scopeID.next;
-        this.selfFunction_ = new FunctionValue(
-            selfFunctionType,
-            [this],
-            Variable(nextScopeID, VariableIndex(1)),
-            nextScopeID,
-            context.save);
-    }
-
     override void execute(scope Context context) pure scope
     in (false)
     {
+        immutable currentFunction = enforce!UnexpectedExecutionException(
+                context.currentFunction);
         immutable argument = enforce!UnmatchTypeException(
                 cast(InductiveValue) context.get(argument_));
         immutable caseVariable = caseFunctions_[cast(size_t) argument.index];
         immutable caseFunction = enforce!UnmatchTypeException(
                 cast(FunctionValue) context.get(caseVariable));
 
-        Rebindable!Value r = caseFunction.execute(selfFunction_);
+        Rebindable!Value r = caseFunction.execute(currentFunction);
         foreach (v; argument.values)
         {
             Value rv = r;
@@ -70,8 +44,29 @@ final immutable class CMatchInstruction : IInstruction
 private:
     Variable argument_;
     immutable(Variable)[] caseFunctions_;
-    FunctionValue selfFunction_;
+
+    /**
+    Constructor.
+
+    Params:
+        argument = match argument
+        caseFunctions = case function variables
+    */
+    this(Variable argument, immutable(Variable)[] caseFunctions) nothrow pure
+    {
+        this.argument_ = argument;
+        this.caseFunctions_ = caseFunctions;
+    }
 }
 
 alias MatchInstruction = immutable(CMatchInstruction);
+
+/**
+Unexpected execution exception.
+*/
+class UnexpectedExecutionException : PoetException
+{
+    ///
+    mixin basicExceptionCtors;
+}
 
