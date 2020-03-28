951ea2926f46fe1120a7b68b0c61538c471dda5f
outlandkarasu <outland.karasu@gmail.com>
2020-03-28 14:49:38 +0900

    issue #19 add inductive constructor to package.

--- source/poet/inductive/package.d
+++ source/poet/inductive/package.d
@@ -7,4 +7,4 @@ public import poet.inductive.create_inductive;
 public import poet.inductive.define_match;
 public import poet.inductive.type;
 public import poet.inductive.value;
-
+public import poet.inductive.push_constructor;
